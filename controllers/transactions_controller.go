1f77af227a591285e81eeecee908c77d2173573e
Your Nameiskandardevan <iskandardevan@student.telkomuniversity.ac.id>
2022-06-17 17:45:42 +0700

    fix transactions

--- controllers/transactions_controller.go
+++ controllers/transactions_controller.go
@@ -26,7 +26,7 @@ func GetTransactionsControllerCode(c echo.Context) error {
 	if transactions.Id == 0 {
 		return c.String(http.StatusNotFound, "transactions not found")
 	}
-	if err := config.DB.Preload("Users").Preload("Benefits").Find(&transactions).Error; err != nil {
+	if err := config.DB.Preload("User").Preload("Benefit").Find(&transactions).Error; err != nil {
 		fmt.Println(err)
 		return c.String(http.StatusInternalServerError, "Internal Server Error")
 	}
@@ -40,27 +40,35 @@ func GetAllTransactionsController(c echo.Context) error {
 		fmt.Println(err)
 		return c.String(http.StatusInternalServerError, "Internal Server Error")
 	}
-	if err := config.DB.Preload("Users").Preload("Benefits").Find(&transactions).Error; err != nil {
+	if err := config.DB.Preload("User").Preload("Benefit").Find(&transactions).Error; err != nil {
 		fmt.Println(err)
 		return c.String(http.StatusInternalServerError, "Internal Server Error")
 	}
 	return c.JSON(http.StatusOK, transactions)
 }
 
-// request POST 'http://127.0.0.1:8080/transactions/'
+// request POST 'http://127.0.0.1:8080/transaction/'
 func CreateTransactionsController(c echo.Context) error {
 	transactions := models.Transactions{}
+
 	if err := c.Bind(&transactions); err != nil {
 		fmt.Println(err)
-		return c.String(http.StatusInternalServerError, "Internal Server Error")
+		return c.String(http.StatusInternalServerError, "Internal Server Error 1")
 	}
-	if err := config.DB.Save(&transactions).Error; err != nil {
+	// Get Benefit By ID
+	var benefit models.Benefits
+	if err := config.DB.First(&benefit, transactions.BenefitId).Error; err != nil {
 		fmt.Println(err)
-		return c.String(http.StatusInternalServerError, "Internal Server Error")
+		return c.String(http.StatusInternalServerError, "Internal Server Error 2")
 	}
-	if err := config.DB.Find(&transactions).Error; err != nil {
+	// Reduce Benefit Stock By 1
+	if err := config.DB.Model(&benefit).Update("stock", benefit.Stock-1).Error; err != nil {
 		fmt.Println(err)
-		return c.String(http.StatusInternalServerError, "Internal Server Error")
+		return c.String(http.StatusInternalServerError, "Internal Server Error 3")
+	}
+	if err := config.DB.Preload("User").Preload("Benefit").Preload("BenefitCategory").Find(&transactions).Error; err != nil {
+		fmt.Println(err)
+		return c.String(http.StatusInternalServerError, "Internal Server Error 3")
 	}
 	return c.JSON(http.StatusOK, transactions)
 }
